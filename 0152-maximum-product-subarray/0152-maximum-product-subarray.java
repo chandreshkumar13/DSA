120e6013f331c22d5c7a0e32054f466c0bd6551c
Chandresh Kumar <chandreshrock7@gmail.com>
2026-09-11 21:30:13 +0530

    Time: 2 ms (71.89%), Space: 47.2 MB (53.17%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int answer = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            int num = nums[i];
+
+            // Negative number ke case mein swap
+            if (num < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+
+            maxProduct = Math.max(num, maxProduct * num);
+            minProduct = Math.min(num, minProduct * num);
+
+            answer = Math.max(answer, maxProduct);
+        }
+
+        return answer;
+    }
+}
